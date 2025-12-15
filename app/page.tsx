13f14fdaf86363e54afc46e43a970f273ae58c9c
indusdynamicyogaorg <indusdynamicyoga.org@gmail.com>
2025-12-16 00:18:33 +0500

    add app directory

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Indus Dynamic Yoga</h1>
+      <p>Website is deploying successfully.</p>
+      <p>Stripe donations will be added next.</p>
+    </main>
+  );
+}
